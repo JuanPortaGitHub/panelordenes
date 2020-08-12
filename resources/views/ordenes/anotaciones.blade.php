f97068682094bcf4a5d97969751b0a8caa366249
Juan <juan_porta@hotmail.com>
2020-08-12 08:42:55 -0300

    Antes buscador anotaciones

--- resources/views/ordenes/anotaciones.blade.php
+++ resources/views/ordenes/anotaciones.blade.php
@@ -2,6 +2,9 @@
 <!-- DataTables -->
 <link rel="stylesheet" href="../adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
 <link rel="stylesheet" href="../adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+
+<title>{{$anotacionOt->ot_id}} | Ordenes de Trabajo</title>
+
 @section('content')
 
 
